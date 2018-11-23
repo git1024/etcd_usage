36f846d833bee8f0c859512fe68a9c87c25c56b4
杜承玖 <duchengjiu@qutoutiao.net>
2018-11-23 11:51:56 +0800

    读取内容详情以及个数,或只读取个数

---
+++ demo3/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		getResp *clientv3.GetResponse
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	kv = clientv3.NewKV(client)
+
+	// 读取内容, 包括个数
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs/job1"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(getResp.Kvs, getResp.Count)
+	}
+
+	// 只读取个数
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs/job1", clientv3.WithCountOnly()); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(getResp.Count)
+	}
+}
