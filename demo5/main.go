41db851b87feba81d1daaffdd3049cd8e0e881a8
杜承玖 <duchengjiu@qutoutiao.net>
2018-11-23 12:02:50 +0800

    删除某个key

---
+++ demo5/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"time"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		delResp *clientv3.DeleteResponse
+		kvpair  *mvccpb.KeyValue
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	kv = clientv3.NewKV(client)
+
+	// 删除KV
+	if delResp, err = kv.Delete(context.TODO(), "/cron/jobs/job1", clientv3.WithFromKey(), clientv3.WithLimit(0)); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	// 被删除之前的value是什么
+	if len(delResp.PrevKvs) != 0 {
+		for _, kvpair = range delResp.PrevKvs {
+			fmt.Println("删除了:", string(kvpair.Key), string(kvpair.Value))
+		}
+	}
+}
